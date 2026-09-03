127187ea594b8bb72426adeec04f6eacbc81033c
Agustin-Franco <agustinfrancoirabedra@gmail.com>
2026-09-03 08:01:59 -0300

    Update 1-WeirdAlgorithm.cpp

--- CSES/1. Introductory Problems/1-WeirdAlgorithm.cpp
+++ CSES/1. Introductory Problems/1-WeirdAlgorithm.cpp
@@ -16,9 +16,22 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) {
 constexpr int MOD = 1e9 + 7;
 
 void solve() {
+  ll n;
+  cin >> n;
+  cout << n << " ";
+  while(n != 1){
+    if(n%2 == 0){
+      n = n/2;
+      cout << n << " ";
+    }
+    else{
+      n = n*3+1;
+      cout << n << " ";
+    }
+  }
 
-  //TODO
-  
+
+
 }
 int main() {
   ios_base::sync_with_stdio(false);
